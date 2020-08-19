f2f3dab2fd6e60b2b3c5b7bcda3c67c21981365d
大卫 <1499782633@qq.com>
2020-08-19 15:44:03 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        'assets': path.resolve(__dirname, 'src/assets'),
+        'components': path.resolve(__dirname, 'src/components'),
+        'views': path.resolve(__dirname, 'src/views'),
+        'commen': path.resolve(__dirname, 'src/commen'),
+        'network': path.resolve(__dirname, 'src/network')
+      }
+    }
+  }
+}
