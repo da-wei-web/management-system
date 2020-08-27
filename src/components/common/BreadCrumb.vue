aa9c7617f198739569fa7a614f0cdfc20f1a3c10
大卫 <1499782633@qq.com>
2020-08-27 11:08:03 +0800

    封装一个面包屑组件并在角色列表组件中引用

---
+++ src/components/common/BreadCrumb.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-breadcrumb :separator-class="icon">
+    <el-breadcrumb-item
+      v-for="(item, index) in titlesList"
+      :key="index"
+      :to="item.path ? { path: item.path } : '' ">
+      {{ item.value }}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+  export default {
+    name: 'BreadCrumb',
+    props: {
+      titlesList: {
+        type: Array,
+        default: []
+      },
+      icon: ''
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/views/role/Role.vue
+++ src/views/role/Role.vue
@@ -1,10 +1,33 @@
 <template>
-  <h2>角色列表</h2>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <BreadCrumb :titles-list="titlesList" icon="el-icon-arrow-right"></BreadCrumb>
+  </el-card>
 </template>
 
 <script>
+  import BreadCrumb from 'components/common/BreadCrumb'
   export default {
-    name: 'Role'
+    name: 'Role',
+    components: {
+      BreadCrumb
+    },
+    data() {
+      return {
+        titlesList: [
+          {
+            value: '首页',
+            path: '/'
+          },
+          {
+            value: '权限管理',
+          },
+          {
+            value: '角色列表'
+          }
+        ]
+      }
+    }
   }
 </script>
 
