ace5a690405c67dabab81c51cd8535c8cae881a6
大卫 <1499782633@qq.com>
2020-08-27 18:51:41 +0800

    在未分配权限的时候在视图上显示提示信息

--- src/views/role/Role.vue
+++ src/views/role/Role.vue
@@ -8,7 +8,7 @@
     <!-- 按钮 -->
     <el-row  class="add-role">
       <el-col>
-        <el-button type="primary">添加角色</el-button>
+        <el-button type="primary" @click="addRole">添加角色</el-button>
       </el-col>
     </el-row>
     <!-- 角色列表展示 -->
@@ -46,6 +46,8 @@
               </el-row>
             </el-col>
           </el-row>
+          <!-- 无权限提示 -->
+          <span v-if="scope.row.children.length === 0">未分配权限</span>
         </template>
       </el-table-column>
       <!-- 第二列-序号 -->
