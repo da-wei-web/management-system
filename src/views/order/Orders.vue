77ee8867caddc36b85d3c2fc35c2899aa7844ca1
大卫 <1499782633@qq.com>
2020-09-14 12:13:41 +0800

    引入面包屑组件

--- src/views/order/Orders.vue
+++ src/views/order/Orders.vue
@@ -1,13 +1,39 @@
 <template>
-  <h2>订单列表</h2>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <bread-crumb :titles-list="titlesList" icon="el-icon-arrow-right" />
+  </el-card>
 </template>
 
 <script>
+  import BreadCrumb from 'components/common/BreadCrumb'
   export default {
-    name: 'Orders'
+    name: 'Orders',
+    components: {
+      BreadCrumb
+    },
+    data() {
+      return {
+        titlesList: [
+          {
+            value: '首页',
+            path: '/'
+          },
+          {
+            value: '订单管理'
+          },
+          {
+            value: '订单列表'
+          }
+        ]
+      }
+    }
   }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .box-card {
+    width: 100%;
+    height: 100%;
+  }
 </style>
