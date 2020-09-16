bcf6f2d6211c4ea2aa237fe26c7c1627e7a6686d
大卫 <1499782633@qq.com>
2020-09-16 20:51:50 +0800

    引入echarts渲染展示数据折线图

--- src/views/reports/DataReport.vue
+++ src/views/reports/DataReport.vue
@@ -1,12 +1,83 @@
 <template>
   <el-card class="box-card">
-    <h2>数据报表</h2>
+    <!-- 面包屑 -->
+    <bread-crumb :titles-list="titlesList" />
+    <!-- 数据折线图 -->
+    <div id="main" style="width: 800px;height:600px;"></div>
   </el-card>
 </template>
 
 <script>
+  import { reportsData } from 'network/reports'
+
+  import BreadCrumb from 'components/common/BreadCrumb'
+
+  import echarts from 'echarts'
   export default {
-    name: 'DataReport'
+    name: 'DataReport',
+    components: {
+      BreadCrumb
+    },
+    data() {
+      return {
+        titlesList: [
+          {
+            value: '首页',
+            path: '/'
+          },
+          {
+            value: '数据统计'
+          },
+          {
+            value: '数据报表'
+          }
+        ],
+        options: {},
+      }
+    },
+    mounted() {
+      this.getReportsData()
+    },
+    methods: {
+      async getReportsData() {
+        const res = await reportsData()
+        
+        const {
+          data,
+          meta: { status },
+        } = res
+
+        console.log(data)
+
+        // 自定义数据
+        let option1 = {
+          title: {
+            text: '堆叠区域图'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985'
+              }
+            }
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+        }
+        if (status === 200) {
+          let option2 = data
+          this.options = {...option1, ...option2}
+          
+          const myChart = echarts.init(document.getElementById('main'))
+          myChart.setOption(this.options)
+        }
+      }
+    }
   }
 </script>
 
@@ -14,5 +85,12 @@
   .box-card {
     width: 100%;
     height: 100%;
+
+    #main {
+      margin-top: 20px;
+    }
   }
 </style>
+
+
+
